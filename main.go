4efa1074cfa994dbb478609d01d09f90e166921d
Muhammad Zahid Masruri <masruri03@gmail.com>
2021-02-18 05:17:52 +0700

    added allowed key to header response

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 	// the w.Write() method to write a "Method Not Allowed" response body. We
 	// then return from the function so that the subsequent code is not executed.
 	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
 		w.WriteHeader(405)
 		w.Write([]byte("Method Not Allowed"))
 		return
