f5a2daada613c4496bac3d75230ef9c6531f5abc
Muhammad Zahid Masruri <masruri03@gmail.com>
2021-02-18 15:36:46 +0700

    refactored using template inheritance

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,10 +20,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+	}
+
 	// Use the template.ParseFiles() function to read the template file into a
 	// template set. If there's an error, we log the detailed error message and use
 	// the http.Error() function to send a generic 500 Internal Server Error
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -38,8 +45,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
-
-	w.Write([]byte("Hello from Snippetbox"))
 }
 
 // Add a showSnippet handler function.
