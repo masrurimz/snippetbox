5d8c1cde8b737275af2081f2e095482b31b1f1ee
Muhammad Zahid Masruri <masruri03@gmail.com>
2021-02-23 12:12:13 +0700

    handled multiple row query for latest snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	// "html/template"
 	"masrurimz/snippetbox/pkg/models"
 	"net/http"
 	"strconv"
@@ -22,30 +22,40 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Initialize a slice containing the paths to the two files. Note that the
-	// home.page.tmpl file must be the *first* file in the slice.
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	ts, err := template.ParseFiles(files...)
+	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	// We then use the Execute() method on the template set to write the template
-	// content as the response body. The last parameter to Execute() represents any
-	// dynamic data that we want to pass in, which for now we'll leave as nil.
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err)
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	// // Initialize a slice containing the paths to the two files. Note that the
+	// // home.page.tmpl file must be the *first* file in the slice.
+	// files := []string{
+	// 	"./ui/html/home.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+
+	// // Use the template.ParseFiles() function to read the template file into a
+	// // template set. If there's an error, we log the detailed error message and use
+	// // the http.Error() function to send a generic 500 Internal Server Error
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// // We then use the Execute() method on the template set to write the template
+	// // content as the response body. The last parameter to Execute() represents any
+	// // dynamic data that we want to pass in, which for now we'll leave as nil.
+	// err = ts.Execute(w, nil)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
 }
 
 // Add a showSnippet handler function.
